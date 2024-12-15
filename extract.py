ef7984dd54397c718cadc97c6bbfbe600065f8ee
B Horn <b@horn.uk>
2024-12-15 10:34:53 +0000

    cleanup, making more independent of target env
    still mainly a single file, but now clear what you have to replace to
    support a new target.
    
    Signed-off-by: B Horn <b@horn.uk>

--- extract.py
+++ extract.py
@@ -3,68 +3,7 @@ import json
 from json import JSONDecoder
 from collections import OrderedDict
 
-# Define all functions for grub.
-setup_code = """
-function not {
-    if [ $1 = 0 ] ; then return 1; fi;
-    return 0
-}
-
-function and {
-    if [ $1 = 0 ] ; then return 0; fi;
-    if [ $2 = 0 ] ; then return 0; fi;
-    return 1
-}
-
-function BUF {
-    set $2=$1
-}
-
-function NOT {
-    not $1
-    set $2=$?
-}
-
-function AND {
-    and $1 $2
-    set $3=$?
-}
-
-function DFF {
-    if [ $1 = 1 ] ; then
-        eval "set $4=\$$3"
-        set $3=$2
-    else
-        eval "set $4=\$$4"
-    fi
-}
-"""
-
-
-class Function:
-    def __init__(self, name, body):
-        self._name = name
-        self._body = self.define(body)
-
-    def __str__(self):
-        return self._body
-
-    def call(self, args=[]):
-        return [' '.join([self._name] + args)]
-
-    def define(self, body):
-        body_str = '    ' + '\n    '.join(body)
-        return f'function {self._name} {{\n{body_str}\n}}'
-
-
-def while_loop(condition, body):
-    body_str = '    ' + '\n    '.join(body)
-    return f'while [ {condition} ] ; do \n{body_str}\ndone'
-
-
-def get_name(name, prefix, v=False):
-    res = f'{prefix}_{name}'
-    return f'${res}' if v else res
+GRUB_CFG_SETUP = './custom/setup.gcfg'
 
 
 class Operation:
@@ -73,22 +12,14 @@ class Operation:
     ARGS_OUT = []
     ARGS_STATE = []
 
-    def __init__(self, args, prefix='w'):
-        base = [self.NAME]
-        for arg in self.ARGS_IN:
-            # if you library is defined correctly this should never fail.
-            assert (len(args[arg]) == 1)
-            base.append(get_name(args[arg][0], prefix, True))
-        for arg in self.ARGS_STATE:
-            base.append(get_name(args[arg][0], prefix + '_state'))
-        for arg in self.ARGS_OUT:
-            assert (len(args[arg]) == 1)
-            base.append(get_name(args[arg][0], prefix))
-
-        self._str_val = ' '.join(base)
+    def __init__(self, args):
+        self._args = args
 
     def state(self):
-        return False
+        return len(self.ARGS_STATE) > 0
+
+    def args(self):
+        return self._args
 
     def __str__(self):
         return self._str_val
@@ -118,85 +49,193 @@ class FlipFlop(Operation):
     ARGS_STATE = ['STATE']
     ARGS_OUT = ['IQ']
 
-    def state(self):
-        return True
 
-
-def map_to_class(op_type, args, prefix='w'):
+def map_to_class(op_type, args):
     match op_type:
         case 'NOT':
-            return Not1(args, prefix=prefix)
+            return Not1(args)
         case 'AND2':
-            return And2(args, prefix=prefix)
+            return And2(args)
         case 'BUF':
-            return Buf(args, prefix=prefix)
+            return Buf(args)
         case 'DFF':
-            return FlipFlop(args, prefix=prefix)
+            return FlipFlop(args)
         case _:
             raise Exception(f'Unimplemented {op_type}')
 
 
-def process_module(mod_name, module, defaults={}):
-    cycle_ops = []
-    state_vars = []
+class Module:
+    """
+    Each instance consisnts of operations, its wires, and the input / output
+    variables we want to read.
 
-    for idx, (_, data) in enumerate(module['cells'].items()):
-        state = {'STATE': [idx]}
-        state.update(data['connections'])
-        op = map_to_class(data['type'], state, prefix=mod_name)
-        if op.state():
-            state_vars.append(idx)
-        cycle_ops.append(op)
+    Alongside this we have state variables for our flip flop implementation.
+    """
+    _states = []
+    _wires = {}
+    _operations = []
+    _variables = {}
+    _clk = None
 
-    stepf = Function(f'step_{mod_name}', map(str, cycle_ops))
+    def __init__(self, name, module, defaults={}):
+        self._name = name
 
+        for idx, (_, data) in enumerate(module['cells'].items()):
+            state = {'STATE': [idx]}
+            state.update(data['connections'])
+            op = map_to_class(data['type'], state)
+            if op.state():
+                self._states.append(idx)
+            self._operations.append(op)
+
+        self._clk = None
+
+        for vname, data in module['netnames'].items():
+            if vname == 'clk':
+                self._clk = data['bits'][0]
+            value = [0]
+            if 'init' in data:
+                value = list(map(int, data['init']))
+            if vname in defaults:
+                value = defaults[vname]
+
+            for port, v in zip(data['bits'], value):
+                self._wires[vname] = {port: v}
+
+            # for printing information
+            if data['hide_name'] == 1:
+                continue
+
+            self._variables[vname] = data['bits']
+
+    def name(self):
+        return self._name
+
+    def states(self):
+        return self._states
+
+    def wires(self):
+        return self._wires
+
+    def operations(self):
+        return self._operations
+
+    def variables(self):
+        return self._variables
+
+    def has_clock(self):
+        return self._clk is not None
+
+    def clock(self):
+        return self._clk
+
+# GRUB specific code
+
+
+class Function:
+    def __init__(self, name, body):
+        self._name = name
+        self._body = self.define(body)
+
+    def __str__(self):
+        return self._body
+
+    def call(self, args=[]):
+        return [' '.join([self._name] + args)]
+
+    def define(self, body):
+        body_str = '    ' + '\n    '.join(body)
+        return f'function {self._name} {{\n{body_str}\n}}'
+
+
+def while_loop(condition, body):
+    body_str = '    ' + '\n    '.join(body)
+    return f'while [ {condition} ] ; do \n{body_str}\ndone'
+
+
+def comment(comment):
+    return f'# {comment}'
+
+
+def echo(line=None):
+    if line:
+        return f'echo {line}'
+    return 'echo'
+
+
+def get_name(name, prefix, v=False):
+    res = f'{prefix}_{name}'
+    return f'${res}' if v else res
+
+
+def op_to_grub(op, prefix):
+    base = [op.NAME]
+    args = op.args()
+
+    for arg in op.ARGS_IN:
+        # if you library is defined correctly this should never fail.
+        assert (len(args[arg]) == 1)
+        base.append(get_name(args[arg][0], prefix, True))
+    for arg in op.ARGS_STATE:
+        base.append(get_name(args[arg][0], prefix + '_state'))
+    for arg in op.ARGS_OUT:
+        assert (len(args[arg]) == 1)
+        base.append(get_name(args[arg][0], prefix))
+
+    return ' '.join(base)
+
+
+def module_to_grub(module, cycle_clk=True):
+    """
+    Map our module class to a grub configuration.
+    """
     body = []
-    body.append(setup_code)
+    with open(GRUB_CFG_SETUP, 'r') as f:
+        body.append(f.read())
 
-    clk = None
+    body.append(comment('Initializing wires'))
+    for wire, ports in module.wires().items():
+        body.append(comment(wire))
+        for port, value in ports.items():
+            tname = get_name(port, module.name())
+            body.append(f'set {tname}={value}')
 
-    # initializing the circuit values
-    for name, data in module['netnames'].items():
-        if name == 'clk':
-            clk = data['bits'][0]
-        value = [0]
-        if 'init' in data:
-            value = list(map(int, data['init']))
-        if name in defaults:
-            value = defaults[name]
-
-        body.append(f'# {name}')
-        for port, v in zip(data['bits'], value):
-            tname = get_name(port, mod_name)
-            body.append(f'set {tname}={v}')
-
-    body.append('# flip flop states')
-    for var in state_vars:
-        tname = get_name(var, mod_name + '_state')
+    body.append(comment('flip flop states'))
+    for var in module.states():
+        tname = get_name(var, module.name() + '_state')
         body.append(f'set {tname}=0')
 
     # main loop
+    stepf = Function(
+        f'step_{module.name()}',
+        map(lambda x: op_to_grub(x, module.name()), module.operations()))
     body.append(str(stepf))
+
     # call once with reset set
     body += stepf.call()
+
     # now the loop body
     loop_body = stepf.call()
-    loop_body.append('echo')
-    for name, data in module['netnames'].items():
-        if data['hide_name'] == 1:
-            continue
+    loop_body.append(echo())
+    for name, bits in module.variables().items():
         rest = ' '.join(
-            map(lambda x: get_name(x, mod_name, True), data['bits'])
+            map(lambda x: get_name(x, module.name(), True), bits)
         )
-        loop_body.append(f'echo {name}: {rest}')
+        loop_body.append(echo(f'{name}: {rest}'))
+
     # cycling the clk
-    if clk:
-        clk_name = get_name(clk, mod_name)
+    if module.has_clock() and cycle_clk:
+        clk_name = get_name(module.clock(), module.name())
         loop_body.append(f'NOT ${clk_name} {clk_name}')
 
     loop_body.append('sleep 1')
     body.append(while_loop('1 = 1', loop_body))
-    print('\n'.join(body))
+    return '\n'.join(body)
+
+
+def process_module(mod_name, module, defaults={}, cycle_clk=True):
+    mod = Module(mod_name, module, defaults)
+    print(module_to_grub(mod, cycle_clk))
 
 
 def main():
